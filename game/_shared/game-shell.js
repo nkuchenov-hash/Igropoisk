99869200b37faf46d142460ee12d1bf7ea443217
nkuchenov-hash <nkuchenov@gmail.com>
2026-08-03 14:14:46 +0300

    Bust cached game page CSS and renderer

--- game/_shared/game-shell.js
+++ game/_shared/game-shell.js
@@ -1,5 +1,9 @@
 (()=>{
+  const style=document.createElement('link');
+  style.rel='stylesheet';
+  style.href='../_shared/game-page.css?v=20260803-4';
+  document.head.appendChild(style);
   const script=document.createElement('script');
-  script.src='../_shared/game-page.js?v=20260803-3';
+  script.src='../_shared/game-page.js?v=20260803-4';
   document.head.appendChild(script);
 })();
