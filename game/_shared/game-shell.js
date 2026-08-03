2445bc7b4de3a8bf85d61ed002c4aa7e428ab249
nkuchenov-hash <nkuchenov@gmail.com>
2026-08-03 14:12:46 +0300

    Bust cached game page renderer

--- game/_shared/game-shell.js
+++ game/_shared/game-shell.js
@@ -1,5 +1,5 @@
 (()=>{
   const script=document.createElement('script');
-  script.src='../_shared/game-page.js';
+  script.src='../_shared/game-page.js?v=20260803-3';
   document.head.appendChild(script);
 })();
