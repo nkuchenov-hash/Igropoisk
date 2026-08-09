7dfc51f7e47e3fc89b9f30da2177ab38e19781c6
nkuchenov-hash <nkuchenov@gmail.com>
2026-08-10 00:42:06 +0300

    Publish complete 78-game catalog reviews

--- game/_shared/game-page.js
+++ game/_shared/game-page.js
@@ -5,4 +5,7 @@
   const editions=document.createElement('script');
   editions.src='../_shared/game-editions.js?v=20260807-1';
   document.head.appendChild(editions);
+  const mediaCategories=document.createElement('script');
+  mediaCategories.src='../_shared/game-media-categories.js?v=20260808-1';
+  document.head.appendChild(mediaCategories);
 })();
